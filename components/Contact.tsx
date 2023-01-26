239d1c6f98407a82e1f204d8316974f1204ff1db
abeonweb <bmospecific@yahoo.com>
2023-01-26 09:04:20 +0100

    fixed class names

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,28 +2,28 @@
 import phone from "../public/phone.svg"
 import location from "../public/location.svg"
 import Image from "next/image";
-// import
+import styles from "@/styles/Contact.module.css"
 
 export default function Contact() {
     return (
-        <section id="contact" className="section section__contact">
-            <div className="contact-container">
-                <div className="contact--info">
-                        <h3 className="contact--info--title">Offices</h3>
-                        <ul className="contact--info--list">
-                            <li><Image src={location} alt="" className="contact--icon" width={20} height={20}/> <p>Gloryland Plaza Apata Ibadan, Oyo</p></li>
-                            <li><Image src={location} alt="" className="contact--icon" width={20} height={20}/> <p>45/47 Karimu Street Surulere, Lagos</p></li>
-                            <li><Image src={location} alt="" className="contact--icon" width={20} height={20}/> <p>773 AMAC Housing Estate Airport Rd, Abuja</p></li>
+        <section id="contact" className={`section ${styles.sectionContact}`}>
+            <div className={styles.contactContainer}>
+                <div className={styles.contactInfo}>
+                        <h3 className={styles.contactInfoTitle}>Offices</h3>
+                        <ul className={styles.contactInfoList}>
+                            <li><Image src={location} alt="" className={styles.contactIcon} width={20} height={20}/> <p>Gloryland Plaza Apata Ibadan, Oyo</p></li>
+                            <li><Image src={location} alt="" className={styles.contactIcon} width={20} height={20}/> <p>45/47 Karimu Street Surulere, Lagos</p></li>
+                            <li><Image src={location} alt="" className={styles.contactIcon} width={20} height={20}/> <p>773 AMAC Housing Estate Airport Rd, Abuja</p></li>
                         </ul>
 
-                        <h3 className="contact--info--title">Mobile</h3>
-                        <ul className="contact--info--list">
-                            <li><Image src={phone} alt="" className="contact--icon" width={20} height={20}/> +234 803 323 7141</li>
-                            <li><Image src={phone} alt="" className="contact--icon" width={20} height={20}/> +234 803 301 7071</li>
-                            <li><Image src={phone} alt="" className="contact--icon" width={20} height={20}/> +234 913 660 6955</li>
+                        <h3 className={styles.contactInfoTitle}>Mobile</h3>
+                        <ul className={styles.contactInfoList}>
+                            <li><Image src={phone} alt="" className={styles.contactIcon} width={20} height={20}/> +234 803 323 7141</li>
+                            <li><Image src={phone} alt="" className={styles.contactIcon} width={20} height={20}/> +234 803 301 7071</li>
+                            <li><Image src={phone} alt="" className={styles.contactIcon} width={20} height={20}/> +234 913 660 6955</li>
                         </ul>
-                    <h3 className="contact--info--title">Office hours</h3>
-                    <p className="office-hours">Mon - Sat: 8:00 AM - 5:00 PM</p>
+                    <h3 className={styles.contactInfoTitle}>Office hours</h3>
+                    <p className={styles.officeHours}>Mon - Sat: 8:00 AM - 5:00 PM</p>
                 </div>
             </div>
         </section>
