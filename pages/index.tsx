efadb038b345330011ad4db14d2928b3333dc2b0
abeonweb <bmospecific@yahoo.com>
2023-01-26 09:03:40 +0100

    added site description to head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,37 @@
-import Head from 'next/head'
-import Features from '../components/Features'
-import Benefits from '../components/Benefits'
-import Form from "../components/Form"
-import Contact from "../components/Contact"
-import Hero from '../components/Hero'
-import Faq from '../components/Faq'
-
+import Head from "next/head";
+import Features from "../components/Features";
+import Benefits from "../components/Benefits";
+import Form from "../components/Form";
+import Contact from "../components/Contact";
+import Hero from "../components/Hero";
+import Faq from "../components/Faq";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Road Rescue & Maintenance Company</title>
-        <meta name="description" content="Road Rescue and Maintenance Company. Specialized in road repair" />
+        <meta
+          name="description"
+          content="Road Rescue and Maintenance Company. Specialized in road repair"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="main">
-            <Hero />
-            <Benefits />
-            <Form />
-            <Features />
-            <Faq />
-            <Contact />
-        </main>
+        <Hero />
+        <Benefits />
+        <Form />
+        <Features />
+        <Faq />
+        <Contact />
+      </main>
     </>
-  )
+  );
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   return {
-    props: {
-
-    }
-  }
-
+    props: {},
+  };
 }
