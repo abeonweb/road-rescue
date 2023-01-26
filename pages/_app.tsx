288d5909459a0d53767d89ebd1b80d1e257c4e7f
abeonweb <bmospecific@yahoo.com>
2023-01-26 08:20:15 +0100

    added font import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
+import { Roboto } from "@next/font/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import SharedLayout from "@/components/SharedLayout";
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100","400", "700", "900"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SharedLayout>
-      <Component {...pageProps} />
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
     </SharedLayout>
   );
 }
