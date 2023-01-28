733bd90418f82fe09c15265697a4ebf51ea4d6e8
abeonweb <bmospecific@yahoo.com>
2023-01-28 11:04:08 +0100

    added size and fill attributes to images

--- components/Objective.tsx
+++ components/Objective.tsx
@@ -9,25 +9,30 @@ type ObjectiveProps = {
   body: string;
   id: number;
 };
-const Objective = ({ image, alt, title, body, id }: ObjectiveProps) => {
+const Objective = ({ image, alt, title, body, id}: ObjectiveProps) => {
   return (
-    <article className={styles.objective}>
-      <div className={styles.objectiveContainer}>
+    <article key={id} className={styles.objective}>
+      <div className={styles.objectiveImageContainer}>
         <Image
           src={`/${image}.webp`}
           alt={alt}
           className={styles.aboutImage}
-          width={300}
-          height={300}
+          priority
+          fill
+          sizes="(min-width: 600px) 100vw, 50vw"
         />
-        <div>
-          <h3 className={styles.aboutBodyTitle}>{title}</h3>
-          <p className={`${styles.aboutBody} ${styles.cutoffText}`}>{body}</p>
+      </div>
+        <div className={styles.objectiveText}>
+          <h3 className={styles.objectiveTitle}>{title}</h3>
+          <p className={`${styles.objectiveBody} ${styles.cutoffText}`}>{body}</p>
           <input type="checkbox" className={styles.expandBtn} placeholder={""}/>
         </div>
-      </div>
     </article>
   );
 };
+
+export async function getStaticProps(){
+
+}
 
 export default Objective;
--- components/VisionCard.tsx
+++ components/VisionCard.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import styles from "@/styles/About.module.css"
+import styles from "@/styles/About.module.css";
 
 type VisionProps = {
   image: string;
@@ -11,20 +11,21 @@ type VisionProps = {
 
 const VisionCard = ({ title, image, alt, text }: VisionProps) => {
   return (
-    <div className={styles.visionCard}>
-      <Image
-        src={`/${image}.webp`}
-        alt={alt}
-        className={styles.visionImage}
-        width={150}
-        height={150}
-        priority
-      />
-      {/* <div className={styles.visionBody}> */}
+    <article className={styles.visionCard}>
+      <div className={styles.visionCardImageContainer}>
+        <Image
+          src={`/${image}.webp`}
+          alt={alt}
+          className={styles.visionImage}
+          fill
+          sizes="(max-width: 780px) 90vw, (max-width: 1100px) 40vw, 30vw"
+        />
+      </div>
+      <div className={styles.visionBody}>
       <h3 className={styles.visionTitle}>{title}</h3>
-      <p className={styles.visionTextBody}>{text}</p>
-      {/* </div> */}
-    </div>
+      <p className={styles.visionText}>{text}</p>
+      </div>
+    </article>
   );
 };
 
